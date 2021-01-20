a405ed8e4e1e84c1f5b0b7dd8d01e2b7f86e4a3b
Buchikin <jirafanegra124@ezweb.ne.jp>
2021-01-20 13:10:21 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze  #半角英数どちらも含む
+  validates_format_of :encrypted_password, with: PASSWORD_REGEX
+
+  validates :nickname, presence: true
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥]/+\z }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥]/+\z }
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]/+\z }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]/+\z }
+  validates :birth_day, presence: true
+
+
 end
