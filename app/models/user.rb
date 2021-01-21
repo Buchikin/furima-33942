dea67170b2daf0157db5c4e027b357654115e355
Buchikin <jirafanegra124@ezweb.ne.jp>
2021-01-21 22:27:31 +0900

    テストコードの追加と整理

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze  #半角英数どちらも含む
-  validates_format_of :password, with: PASSWORD_REGEX, message: "must be included both letters and numbers"
+  validates_format_of :password, with: PASSWORD_REGEX, message: "must be included both letter(Half-width) and number"
 
   with_options presence: true do
     validates :nickname
