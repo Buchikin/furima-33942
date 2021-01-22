d64b3d695abe1d9e7719ea0a17301e028be0e68c
Buchikin <jirafanegra124@ezweb.ne.jp>
2021-01-22 18:57:33 +0900

    出品機能のコントローラー実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,11 +5,27 @@ class ItemsController < ApplicationController
     
   end
 
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
 
   private
   def move_to_new_user_session
     unless user_signed_in?
       redirect_to new_user_session_path
     end
+  end
+
+  def item_params
+    params.require(:item).permit(:name, :info, :category_id, :condition_id, :charge_id, :prefecture_id, :ship_day_id, :price).merge(user_id: current_user.id)
   end
 end
