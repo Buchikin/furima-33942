4fa3c54f8e65d0c44bc2b095c93d99118df842ba
Buchikin <jirafanegra124@ezweb.ne.jp>
2021-01-22 18:31:44 +0900

    出品ページへの遷移の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,15 @@
 class ItemsController < ApplicationController
+  before_action :move_to_new_user_session, only: [:new]
+
   def index
     
+  end
+
+
+  private
+  def move_to_new_user_session
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
   end
 end
