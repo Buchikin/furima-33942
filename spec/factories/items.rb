d45d47d589d0a5efb964e8283f92919374f2752f
Buchikin <jirafanegra124@ezweb.ne.jp>
2021-01-23 19:37:08 +0900

    itemモデルの単体テストコードの実装

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    
+    name {Faker::Lorem.word}
+    info {Faker::Lorem.sentence}
+    price {Random.rand(300 .. 9999999)}
+    category_id {Random.rand(1 .. 10)}
+    condition_id {Random.rand(1 .. 6)}
+    charge_id {Random.rand(1 .. 2)}
+    prefecture_id {Random.rand(1 .. 47)}
+    ship_day_id {Random.rand(1 .. 3)}
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
