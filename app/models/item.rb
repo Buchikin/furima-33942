2c254b833753f865e60012b73fc2eba0350fb0c5
Buchikin <jirafanegra124@ezweb.ne.jp>
2021-01-23 19:56:19 +0900

    バリデーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,8 +5,6 @@ class Item < ApplicationRecord
     validates :name
     validates :info
     validates :price, numericality: { :greater_than_or_equal_to => 300 , :less_than_or_equal_to => 9999999 , message:"must be 300~9999999" }
-    validates :price, numericality: { with: /\A[0-9]+\z/, message: "must be Half-width numbers"}
-    validates :price, numericality: { only_integer: true, message: "must be Integer"}
 
     with_options numericality: { other_than: 0 , message: "must be selected"} do
       validates :category_id
@@ -16,6 +14,9 @@ class Item < ApplicationRecord
       validates :ship_day_id
     end
   end
+
+  validates :price, numericality: { with: /\A[0-9]+\z/, message: "must be Half-width numbers"}
+  validates :price, numericality: { only_integer: true, message: "must be Integer"}
   
   belongs_to :user
   has_one_attached :image
