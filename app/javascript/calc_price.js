0672a04920b7e24c91e8afdebfa3d833a7efa075
Buchikin <jirafanegra124@ezweb.ne.jp>
2021-01-25 11:17:40 +0900

    販売手数料・販売利益が表示されるようのjavscriptの修正

--- app/javascript/calc_price.js
+++ app/javascript/calc_price.js
@@ -1,13 +1,20 @@
 function calc_price (){
   const price = document.getElementById("item-price");
+  const taxPrice = document.getElementById("add-tax-price");
+  const profit = document.getElementById("profit");
+
+  function calc (){
+    const tax = Math.floor(price.value * 0.1);
+    const gain = Number(price.value) - tax;
+    taxPrice.innerHTML = `${tax}`;
+    profit.innerHTML = `${gain}`;
+  };
+
+  calc() ;
+
   price.addEventListener("keyup", () => {
-    const taxPrice = document.getElementById("add-tax-price");
-    const profit = document.getElementById("profit");
     if (Number.isInteger(Number(price.value))){
-      const tax = Math.floor(price.value * 0.1);
-      const gain = Number(price.value) - tax;
-      taxPrice.innerHTML = `${tax}`;
-      profit.innerHTML = `${gain}`;
+      calc();
     } else {
       taxPrice.innerHTML = "半角整数で入力";
       profit.innerHTML = "半角整数で入力";
