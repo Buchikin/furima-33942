b101a7bae3610fbfee479e11425472171ff9c4cc
Buchikin <jirafanegra124@ezweb.ne.jp>
2021-01-26 15:29:22 +0900

    Revert "クレジットカード決済のためのcardモデルの作成とテーブルの作成"
    This reverts commit c37ab06f8938ad44db8a5e2d5ca64f7dcd8c24e5.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_26_062433) do
+ActiveRecord::Schema.define(version: 2021_01_26_015550) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -44,12 +44,6 @@ ActiveRecord::Schema.define(version: 2021_01_26_062433) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["purchase_id"], name: "index_addresses_on_purchase_id"
-  end
-
-  create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.integer "price", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
