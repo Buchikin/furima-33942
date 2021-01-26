201395a9dfa4dc6c3494f0ff076a390c2f3adf0b
Buchikin <jirafanegra124@ezweb.ne.jp>
2021-01-26 11:46:52 +0900

    バリデーションの修正

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,7 +4,7 @@ class Order
 
   with_options presence: true do
     validates :zip, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "must be Half-width numbers and included hyphen(-)" }
-    validates :prefecture, numericality: { other_than: 0, message: "must be selected" }
+    validates :prefecture_id, numericality: { other_than: 0, message: "must be selected" }
     validates :city
     validates :house_num
     validates :phone_num, numericality: { with: /\A[0-9]+\z/, message: "must be Half-width numbers"}
