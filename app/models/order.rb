afa6e9f25af190cc2115bf5c69ac4ee1556a5e59
Buchikin <jirafanegra124@ezweb.ne.jp>
2021-01-27 15:17:10 +0900

    バリデーションの修正

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,13 +1,13 @@
 class Order
   include ActiveModel::Model
-  attr_accessor :zip, :prefecture_id, :city, :house_num, :building, :phone_num, :user_id, :item_id, :price, :token
+  attr_accessor :zip, :prefecture_id, :city, :house_num, :building, :phone_num, :user_id, :item_id, :token
 
   with_options presence: true do
     validates :zip, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "must be Half-width numbers and included hyphen(-)" }
     validates :prefecture_id, numericality: { other_than: 0, message: "must be selected" }
     validates :city
     validates :house_num
-    validates :phone_num, numericality: { with: /\A[0-9]+\z/, message: "must be Half-width numbers"}
+    validates :phone_num, numericality: { with: /\A[0-9]{,12}+\z/, message: "must be Half-width 12 numbers"}
     validates :token
     validates :user_id
     validates :item_id
